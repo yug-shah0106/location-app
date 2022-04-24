6bf1ecb70ba8a81124bf047aacd9afefc3a8e549
yug shah <yugshah0106@gmail.com>
2022-04-24 14:34:22 +0530

    node_modules removed.

--- client/node_modules/react-native-calendars/src/componentUpdater.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function shouldUpdate(props: any, newProps: any, paths: string[]): boolean;
-export declare function extractComponentProps(component: any, props: any, ignoreProps?: string[]): any;
--- client/node_modules/react-native-calendars/src/timeline/NowIndicator.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { TextStyle, ViewStyle } from 'react-native';
-export interface NowIndicatorProps {
-    styles: {
-        [key: string]: ViewStyle | TextStyle;
-    };
-}
-declare const NowIndicator: (props: NowIndicatorProps) => JSX.Element;
-export default NowIndicator;
